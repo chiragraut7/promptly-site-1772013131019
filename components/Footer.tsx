736297c167924ed28319ac58da8180fd86b1f6bb
chiragraut7 <chiragraut7@gmail.com>
2026-02-25 15:22:17 +0530

    Add components/Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-white shadow-md py-4">
+      <div className="container mx-auto text-center text-gray-600">
+        &copy; 2024 Admin Panel. All rights reserved.
+      </div>
+    </footer>
+  );
+}
